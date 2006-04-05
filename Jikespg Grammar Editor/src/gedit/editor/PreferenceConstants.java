ddb9d7cd883dfe4dd0b48c858785d03f1c726f79
Uwe Voigt <>
2006-04-05 07:59:53 +0000

    added two keys

--- Jikespg Grammar Editor/src/gedit/editor/PreferenceConstants.java
+++ Jikespg Grammar Editor/src/gedit/editor/PreferenceConstants.java
@@ -10,11 +10,13 @@ public class PreferenceConstants {
 	public static final String EDITOR_FOLDING_ENABLED = "folding_enabled";
 	public static final String EDITOR_FOLD_SECTIONS = "fold_sections";
 	public static final String EDITOR_FOLD_RULES = "fold_rules";
+	public static final String EDITOR_FOLD_COMMENTS = "fold_comments";
 	public static final String EDITOR_BOLD_SUFFIX = "_bold";
 	public static final String EDITOR_ITALIC_SUFFIX = "_italic";
 	public static final String EDITOR_STRIKETHROUGH_SUFFIX = "_striketrough";
 	public static final String EDITOR_UNDERLINE_SUFFIX = "_underline";
 	public static final String EDITOR_MAXIMUM_PROBLEMS_REPORTED = "maximum_problems_reported";
+	public static final String EDITOR_MARK_OCCURRENCES = "mark_occurrences";
 	public static final String GRAMMAR_COLORING_COMMENT = "color_comment";
 	public static final String GRAMMAR_COLORING_OPTION = "color_option";
 	public static final String GRAMMAR_COLORING_MACRO = "color_macro";
