40b21c25b2719de688ab628eddfc37b35f885890
Uwe Voigt <>
2006-07-28 13:14:01 +0000

    added quoted detection

--- Jikespg Grammar Editor/src/gedit/editor/GrammarScanner.java
+++ Jikespg Grammar Editor/src/gedit/editor/GrammarScanner.java
@@ -5,6 +5,7 @@
 package gedit.editor;
 
 import gedit.GrammarEditorPlugin;
+import gedit.model.Document;
 
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.rules.IRule;
@@ -13,7 +14,6 @@ import org.eclipse.jface.text.rules.IWhitespaceDetector;
 import org.eclipse.jface.text.rules.RuleBasedScanner;
 import org.eclipse.jface.text.rules.Token;
 import org.eclipse.jface.text.rules.WhitespaceRule;
-import org.eclipse.jface.text.rules.WordRule;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
 
@@ -27,22 +27,30 @@ public class GrammarScanner extends RuleBasedScanner {
 
 	private String fPreferenceKey;
 	private PreferenceUtils fUtils;
-	private MacroKeyDetector fMacroKeyDetector;
+	private MacroKeyRule fMacroKeyRule;
+	private boolean fDetectOperators;
+	private boolean fDetectQuotes;
 	
-	public GrammarScanner(String preferenceKey, PreferenceUtils utils) {
+	public GrammarScanner(String preferenceKey, PreferenceUtils utils, boolean detectOperators, boolean detectQuotes) {
 		fPreferenceKey = preferenceKey;
 		fUtils = utils;
+		fDetectOperators = detectOperators;
+		fDetectQuotes = detectQuotes;
 
 		setRules();
 	}
 	
 	private void setRules() {
 		IToken macroKeyToken = new Token(fUtils.createTextAttribute(PreferenceConstants.GRAMMAR_COLORING_MACRO_KEY));
+		IToken operator = new Token(fUtils.createTextAttribute(PreferenceConstants.GRAMMAR_COLORING_OPERATOR));
 
-		IRule[] rules = {
-				new WordRule(fMacroKeyDetector = new MacroKeyDetector(), macroKeyToken),
-				new WhitespaceRule(new WhitespaceDetector()),
-		};
+		IRule[] rules = new IRule[fDetectOperators ? 3 : 2];
+		rules[0] = fMacroKeyRule = new MacroKeyRule(new MacroKeyDetector(), macroKeyToken);
+		rules[1] = new WhitespaceRule(new WhitespaceDetector());
+		if (fDetectOperators)
+			rules[2] = new OperatorRule(operator);
+		if (!fDetectQuotes)
+			fMacroKeyRule.getMacroKeyDetector().setQuoteChars(new char[0]);
 
 		setRules(rules);
 
@@ -52,7 +60,14 @@ public class GrammarScanner extends RuleBasedScanner {
 	
 	public void setRange(IDocument document, int offset, int length) {
 		super.setRange(document, offset, length);
-		fMacroKeyDetector.setEscape(GrammarEditorPlugin.getDocumentModel(document, null, false).getEsape());
+		Document model = GrammarEditorPlugin.getDocumentModel(document, null, false);
+		fMacroKeyRule.getMacroKeyDetector().setEscape(model.getOptions().getEsape());
+		fMacroKeyRule.getMacroKeyDetector().setMakros(model.getAllMakros());
+		if (fDetectOperators) {
+			OperatorRule operatorRule = (OperatorRule) fRules[2];
+			operatorRule.reset();
+			operatorRule.setOrMarker(model.getOptions().getOrMarker());
+		}
 	}
 	
 	public void adaptToPreferenceChange(PropertyChangeEvent event) {
@@ -65,10 +80,12 @@ public class GrammarScanner extends RuleBasedScanner {
 		if (fPreferenceKey == null)
 			return AbstractTextEditor.PREFERENCE_COLOR_FOREGROUND.equals(property) ||
 					AbstractTextEditor.PREFERENCE_COLOR_FOREGROUND_SYSTEM_DEFAULT.equals(property) ||
-					property != null && property.startsWith(PreferenceConstants.GRAMMAR_COLORING_MACRO_KEY);
+					property != null && (property.startsWith(PreferenceConstants.GRAMMAR_COLORING_MACRO_KEY) ||
+							property.startsWith(PreferenceConstants.GRAMMAR_COLORING_OPERATOR));
 		return property != null && (
 				property.startsWith(PreferenceConstants.GRAMMAR_COLORING_MACRO_KEY) ||
-				property.startsWith(fPreferenceKey));
+				property.startsWith(fPreferenceKey) ||
+				property.startsWith(PreferenceConstants.GRAMMAR_COLORING_OPERATOR));
 	}
 	
 }
