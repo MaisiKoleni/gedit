9b66a2b373a1e228ac764cb3402f6d4751f2a98b
Uwe Voigt <>
2006-07-28 12:52:10 +0000

    added quoted detection

--- Jikespg Grammar Editor/src/gedit/editor/CaseInsensitiveSingleLineRule.java
+++ Jikespg Grammar Editor/src/gedit/editor/CaseInsensitiveSingleLineRule.java
@@ -4,23 +4,53 @@
  */
 package gedit.editor;
 
+import gedit.StringUtils.QuoteDetector;
+
 import org.eclipse.jface.text.rules.ICharacterScanner;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.SingleLineRule;
+import org.eclipse.jface.text.rules.Token;
 
 public class CaseInsensitiveSingleLineRule extends SingleLineRule {
+	private QuoteDetector fQuoteDetector;
+
 	public CaseInsensitiveSingleLineRule(String startSequence, String endSequence, IToken token, char escapeCharacter, boolean breaksOnEOF) {
 		super(startSequence, endSequence, token, escapeCharacter, breaksOnEOF);
+		fQuoteDetector = new QuoteDetector();
 		for (int i = 0; i < fStartSequence.length; i++)
 			fStartSequence[i] = Character.toLowerCase(fStartSequence[i]);
+	}
+
+	protected IToken doEvaluate(ICharacterScanner scanner, boolean resume) {
+
+		if (resume) {
+
+			if (endSequenceDetected(scanner))
+				return fToken;
+
+		} else {
+
+			int c= scanner.read();
+			fQuoteDetector.detect(c);
+			if (c == fStartSequence[0]) {
+				if (sequenceDetected(scanner, fStartSequence, false)) {
+					if (endSequenceDetected(scanner))
+						return fToken;
+				}
+			}
+		}
+
+		scanner.unread();
+		return Token.UNDEFINED;
 	}
 
 	protected boolean sequenceDetected(ICharacterScanner scanner, char[] sequence, boolean eofAllowed) {
 		for (int i= 1; i < sequence.length; i++) {
 			int c= scanner.read();
-			if (c == ICharacterScanner.EOF && eofAllowed) {
+			if (c == ICharacterScanner.EOF && eofAllowed)
 				return true;
-			} else if (Character.toLowerCase((char) c) != sequence[i]) {
+
+			if (fQuoteDetector.isInQuotes() || Character.toLowerCase((char) c) != sequence[i]) {
 				// Non-matching character detected, rewind the scanner back to the start.
 				// Do not unread the first character.
 				scanner.unread();
@@ -31,5 +61,9 @@ public class CaseInsensitiveSingleLineRule extends SingleLineRule {
 		}
 
 		return true;
+	}
+	
+	public void reset() {
+		fQuoteDetector.reset();
 	}
 }
