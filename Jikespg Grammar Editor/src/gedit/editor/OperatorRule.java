bca79770f0c6eb7e00221c7750fa9dde915f9244
Uwe Voigt <>
2006-07-28 13:19:10 +0000

    added quote detection

--- Jikespg Grammar Editor/src/gedit/editor/OperatorRule.java
+++ Jikespg Grammar Editor/src/gedit/editor/OperatorRule.java
@@ -4,6 +4,8 @@
  */
 package gedit.editor;
 
+import gedit.StringUtils.QuoteDetector;
+
 import org.eclipse.jface.text.rules.ICharacterScanner;
 import org.eclipse.jface.text.rules.IPredicateRule;
 import org.eclipse.jface.text.rules.IToken;
@@ -11,9 +13,12 @@ import org.eclipse.jface.text.rules.Token;
 
 public class OperatorRule implements IPredicateRule {
 	private IToken fToken;
+	private QuoteDetector fQuoteDetector;
+	private char fOrMarker;
 
 	public OperatorRule(IToken token) {
 		fToken = token;
+		fQuoteDetector = new QuoteDetector();
 	}
 
 	private IToken unread(ICharacterScanner scanner, int num) {
@@ -27,23 +32,69 @@ public class OperatorRule implements IPredicateRule {
 	}
 
 	public IToken evaluate(ICharacterScanner scanner) {
-		switch (scanner.read()) {
-		case ':':
-			if (scanner.read() != ':')
-				return unread(scanner, 2);
-			if (scanner.read() == '=')
-				return fToken;
-			return unread(scanner, 3);
-		case '-':
-			if (scanner.read() == '>')
-				return fToken;
-			return unread(scanner, 2);
+		int c = scanner.read();
+		fQuoteDetector.detect(c);
+		if (!fQuoteDetector.isInQuotes()) {
+			IToken token = doEvaluate(c, scanner);
+			if (token != null)
+				return token;
 		}
 		return unread(scanner, 1);
 	}
 	
+	private IToken doEvaluate(int c, ICharacterScanner scanner) {
+		switch (c) {
+		default:
+			if (c == fOrMarker) {
+				if (!Character.isWhitespace((char) scanner.read()))
+					return unread(scanner, 2);
+				scanner.unread();
+				return fToken;
+			} else
+				return null;
+		case ':':
+			if (scanner.read() != ':')
+				return unread(scanner, 2);
+			if (scanner.read() != '=')
+				return unread(scanner, 3);
+			c = scanner.read();
+			if (c == '?') {
+				if (!Character.isWhitespace((char) scanner.read()))
+					return unread(scanner, 5);
+				scanner.unread();
+				return fToken;
+			} else if (Character.isWhitespace((char) c)) {
+				scanner.unread();
+				return fToken;
+			}
+			return unread(scanner, 4);
+		case '-':
+			if (scanner.read() != '>')
+				return unread(scanner, 2);
+			c = scanner.read();
+			if (c == '?') {
+				if (!Character.isWhitespace((char) scanner.read()))
+					return unread(scanner, 4);
+				scanner.unread();
+				return fToken;
+			} else if (Character.isWhitespace((char) c)) {
+				scanner.unread();
+				return fToken;
+			}
+			return unread(scanner, 3);
+		}
+	}
+
 	public IToken getSuccessToken() {
 		return fToken;
+	}
+	
+	public void reset() {
+		fQuoteDetector.reset();
+	}
+
+	public void setOrMarker(char orMarker) {
+		fOrMarker = orMarker;
 	}
 	
 }
